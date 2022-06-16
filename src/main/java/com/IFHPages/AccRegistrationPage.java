1322ae81a99b47cc8113a41768f6d1e8b6698bbb
Ashok <ashoklucky577@gmail.com>
2022-06-16 15:13:36 +0530

    Added new test

---
+++ src/main/java/com/IFHPages/AccRegistrationPage.java
@@ -0,0 +1,49 @@
+package com.IFHPages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.BaseActionClass.Action;
+import com.BaseClass.BaseTest;
+import com.BaseClass.Config_Properties;
+import com.BaseClass.FileLib;
+import com.BaseClass.IAutoConsts;
+
+public class AccRegistrationPage extends Config_Properties {
+
+	private @FindBy(xpath = "//button[@id='ifhItem0']") WebElement clickOnFAQsIcon;
+	private @FindBy(xpath = "//button[@id='ifhFaqControlOC2766']") WebElement clickOnFAQs_1_Que;
+	private @FindBy(xpath = "//button[@title='Experiencing issues?']") WebElement clickOnSuerveyIcon;
+	private @FindBy(xpath = "//textarea[@id='ifhTextArea_0']")WebElement enterText;
+	private @FindBy(xpath="//input[@id='businessaccount-radio']") WebElement businessRadioButton;
+
+	public AccRegistrationPage() {
+		PageFactory.initElements(BaseTest.driver, this);
+	}
+
+	public  void personalAccRegPage() throws Throwable {
+
+		loadConfigValues();
+		Action.getUrl(REG_US_URL);
+		Action.explicitWaitForElement(BaseTest.driver, clickOnFAQsIcon, 20);
+		Action.click(clickOnFAQsIcon, "FAQs Icon");
+		Action.click(clickOnFAQs_1_Que, "FAQs 1st");
+		Action.click(clickOnSuerveyIcon, "Suervey Icon");
+		Action.type(enterText, "Written text is for testing purposes");
+
+	}
+	
+	public void businessAccRegPage() throws Throwable {
+
+		loadConfigValues();
+		Action.getUrl(REG_US_URL);
+		Action.explicitWaitForElement(BaseTest.driver, clickOnFAQsIcon, 20);
+		Action.click(businessRadioButton, "Click on Bussines Radio Button");
+		Action.click(clickOnFAQsIcon, "FAQs Icon");
+		Action.click(clickOnFAQs_1_Que, "FAQs 1st");
+		Action.click(clickOnSuerveyIcon, "Suervey Icon");
+		Action.type(enterText, "Written text is for testing purposes");
+
+	}
+}
--- src/test/java/com/IFHTests/RegistrationPageTest.java
+++ src/test/java/com/IFHTests/RegistrationPageTest.java
@@ -1,16 +1,24 @@
 package com.IFHTests;
 
-import org.testng.annotations.Parameters;
+
 import org.testng.annotations.Test;
 
 import com.BaseClass.BaseTest;
-import com.IFHPages.PersonalAccRegistrationPage;
+import com.IFHPages.AccRegistrationPage;
+
+
 
 public class RegistrationPageTest extends BaseTest{
 	
 	@Test
-	public void PersonalAccRegistrationPageTest() throws Throwable {
-		new PersonalAccRegistrationPage().PersonalAccRegPage();
+	public void personalAccRegistrationPageTest() throws Throwable {
+		new AccRegistrationPage().personalAccRegPage();
 	}
+	
+	@Test
+	public void businessAccRegistrationPageTest() throws Throwable {
+		new AccRegistrationPage().businessAccRegPage();
+	}
+	
 	
 }
